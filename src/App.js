c6775956328db292ba0a9bbe6358b7270d928dba
Renato Santana <renatosantana@Renatos-MacBook-Air.local>
2021-09-22 07:26:38 -0300

    rebuilt using yarn and npx

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world. This is my first react app!</h1>
+        <h1>Hello World!!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
