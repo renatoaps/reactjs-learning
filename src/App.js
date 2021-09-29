e263264e6fab46e8deb561cc7ecc35416cd73533
Renato Santana <renatodev@icloud.com>
2021-09-29 07:01:03 -0300

    file structure changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Clock from "./drafts/Clock";
-import Author from "./drafts/Author";
+import Clock from "./components/Clock";
+import Author from "./components/Author";
 import React from "react";
 
 function App() {
